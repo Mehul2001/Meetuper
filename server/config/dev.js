7596c065cbc255efecb54b53309f7fdd7097b02c
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-04-13 17:36:58 -0700

    Basic Setup

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB_URI: 'mongodb+srv://harsh:testtest@cluster0-uife0.mongodb.net/test?retryWrites=true'
+}
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import AppDropdown from './components/shared/AppDropdown'
+import AppHero from './components/shared/AppHero'
+
+Vue.config.productionTip = false
+
+Vue.component('AppHero', AppHero)
+Vue.component('AppDropdown', AppDropdown)
+
+Vue.filter('capitalize', function (value) {
+  if (value && typeof value === 'string') {
+    return value.charAt(0).toUpperCase() + value.slice(1)
+  }
+
+  return ''
+})
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
