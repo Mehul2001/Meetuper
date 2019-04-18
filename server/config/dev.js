12a34682b981d69c7709599dd06420b323c45556
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-04-18 13:43:27 -0700

    Add session authentication on server side

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-    DB_URI: 'mongodb+srv://harsh:testtest@cluster0-uife0.mongodb.net/test?retryWrites=true'
+    DB_URI: 'mongodb+srv://harsh:testtest@cluster0-uife0.mongodb.net/test?retryWrites=true',
+    SESSION_SECRET: 'asddfggghh'
 }
